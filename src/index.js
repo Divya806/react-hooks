2eb215cc962df5c0461864c1c5773c912108817e
Ramalingappa <divya.ramalingappa@accenture.com>
2023-03-06 19:37:27 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import HookCounter from './Components/HookCounter';
-import StateCounter from './Components/StateCounter';
-import StateCounter2 from './Components/StateCounter2';
-import Name from './Components/Name';
+// import HookCounter from './Components/HookCounter';
+// import StateCounter from './Components/StateCounter';
+// import StateCounter2 from './Components/StateCounter2';
+// import Name from './Components/Name';
+// import HookCounterFour from './Components/HookCounterFour';
+import SideEffects from './Components/SideEffects';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <HookCounter />
+    {/* <HookCounter />
     <StateCounter />
-    <StateCounter2 />
-    <Name />
+    <StateCounter2 /> */}
+    {/* <Name /> */}
+    {/* <HookCounterFour /> */}
+    <SideEffects />
   </React.StrictMode>
 );
 
